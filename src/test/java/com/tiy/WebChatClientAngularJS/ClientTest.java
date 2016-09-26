d81bb611d427d8abc0d0428e0a5edbd3ec3d1053
Clayton Strickland <cjstrickland5@gmail.com>
2016-09-25 21:04:50 -0400

    Added Test

---
+++ src/test/java/com/tiy/WebChatClientAngularJS/ClientTest.java
@@ -0,0 +1,56 @@
+package com.tiy.WebChatClientAngularJS;
+import jodd.json.JsonSerializer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+
+public class ClientTest {
+    @Autowired
+    MessageRepository messages;
+
+    @Before
+    public void setUp() throws Exception {
+//        Server myServer = new Server();
+//        myServer.startServer();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void sendUserMessageTest() throws Exception {
+        Client myClient = new Client();
+        myClient.createNewClientSocket();
+
+        String testText = "test.user.message";
+        String testUsername = "test.user.name";
+        Message testMessage = new Message(testUsername, testText);
+        String testMessageSerialized = jsonSave(testMessage);
+        ArrayList<String> serverResponses = myClient.sendUserMessage(testMessageSerialized);
+        assertEquals(7, serverResponses.size());
+        assertEquals(testUsername + " said: " + testText, serverResponses.get(0));
+
+        //delete testMessage from the db
+        messages.delete(testMessage);
+    }
+
+    public String jsonSave(Message messageToSave) {
+        JsonSerializer jsonSerializer = new JsonSerializer().deep(true);
+        String jsonString = jsonSerializer.serialize(messageToSave);
+
+        return jsonString;
+    }
+
+}
